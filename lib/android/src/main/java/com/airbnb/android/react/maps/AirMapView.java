b9aaf933e4b02a1ae8fb743cdcd62f7f438a921c
Pedro Lopes <pedrolopes10@gmail.com>
2018-06-29 17:49:15 +0100

    fix typo miss

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapView.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapView.java
@@ -612,6 +612,7 @@ public class AirMapView extends MapView implements GoogleMap.InfoWindowAdapter,
     }
   }
 
+
   public void animateToNavigation(LatLng location, float bearing, float angle, int duration) {
     if (map == null) return;
     CameraPosition cameraPosition = new CameraPosition.Builder(map.getCameraPosition())
@@ -622,7 +623,7 @@ public class AirMapView extends MapView implements GoogleMap.InfoWindowAdapter,
     map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), duration, null);
   }
 
-  public void animateToRegion(LatLngBounds bounds, int duration, ReadableMap edgePadding)
+  public void animateToRegion(LatLngBounds bounds, int duration, ReadableMap edgePadding) {
     if (map == null) return;
 
     if (edgePadding != null) {
