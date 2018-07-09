5b658992aab811662b69379ad1d47a5cd6a9dc9d
pedrolopes10 <pedrolopes10@gmail.com>
2018-07-09 20:52:44 +0100

    multiple segments gradient polyline

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapPolyline.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapPolyline.java
@@ -1,6 +1,7 @@
 package com.airbnb.android.react.maps;
 
 import android.content.Context;
+import android.graphics.Color;
 
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
@@ -14,10 +15,13 @@ import java.util.List;
 
 public class AirMapPolyline extends AirMapFeature {
 
-  private PolylineOptions polylineOptions;
+  private List<Polyline> polylineArray;
   private Polyline polyline;
 
+  private GoogleMap map;
+
   private List<LatLng> coordinates;
+  private List<String> strokeColors;
   private int color;
   private float width;
   private boolean geodesic;
@@ -27,6 +31,16 @@ public class AirMapPolyline extends AirMapFeature {
     super(context);
   }
 
+  public void setStrokeColors(ReadableArray strokeColors) {
+    if(strokeColors != null){
+        this.strokeColors = new ArrayList<>(strokeColors.size());
+        for (int i = 0; i < strokeColors.size(); i++) {
+          String strokeColor = strokeColors.getString(i);
+          this.strokeColors.add(i, strokeColor);
+        }
+    }
+  }
+
   public void setCoordinates(ReadableArray coordinates) {
     this.coordinates = new ArrayList<>(coordinates.size());
     for (int i = 0; i < coordinates.size(); i++) {
@@ -34,54 +48,91 @@ public class AirMapPolyline extends AirMapFeature {
       this.coordinates.add(i,
           new LatLng(coordinate.getDouble("latitude"), coordinate.getDouble("longitude")));
     }
-    if (polyline != null) {
-      polyline.setPoints(this.coordinates);
+
+    if (polylineArray != null) {
+        if(this.polylineArray.size() == 1){
+            polyline.setPoints(this.coordinates);
+        } else {
+            PolylineOptions options = new PolylineOptions();
+
+            if(polylineArray.size() > coordinates.size()){
+                removeFromMap(this.map);
+                createPolyline();
+            } else {
+                if(coordinates.size() > 1){
+                   ReadableMap coordinate2 = coordinates.getMap(coordinates.size()-2);
+                   options.add(new LatLng(coordinate2.getDouble("latitude"), coordinate2.getDouble("longitude")));
+                }
+
+                ReadableMap coordinate = coordinates.getMap(coordinates.size()-1);
+                options.add(new LatLng(coordinate.getDouble("latitude"), coordinate.getDouble("longitude")));
+
+                Integer colorToUse;
+
+                if(this.strokeColors != null){
+                    colorToUse = Color.parseColor(this.strokeColors.get(this.strokeColors.size()-1));
+                } else {
+                    colorToUse = color;
+                }
+
+                options.color(colorToUse);
+                options.width(width);
+                options.geodesic(geodesic);
+                options.zIndex(zIndex);
+
+                polyline = this.map.addPolyline(options);
+                polyline.setClickable(true);
+
+                this.polylineArray.add(polyline);
+          }
+        }
     }
   }
 
   public void setColor(int color) {
     this.color = color;
-    if (polyline != null) {
-      polyline.setColor(color);
-    }
   }
 
   public void setWidth(float width) {
     this.width = width;
-    if (polyline != null) {
-      polyline.setWidth(width);
-    }
   }
 
   public void setZIndex(float zIndex) {
     this.zIndex = zIndex;
-    if (polyline != null) {
-      polyline.setZIndex(zIndex);
-    }
   }
 
   public void setGeodesic(boolean geodesic) {
     this.geodesic = geodesic;
-    if (polyline != null) {
-      polyline.setGeodesic(geodesic);
-    }
   }
 
-  public PolylineOptions getPolylineOptions() {
-    if (polylineOptions == null) {
-      polylineOptions = createPolylineOptions();
-    }
-    return polylineOptions;
-  }
+  private void createPolyline() {
+    this.polylineArray = new ArrayList<>(coordinates.size());
+    for (int i = 0; i < coordinates.size()-1; i++) {
+        PolylineOptions options = new PolylineOptions();
 
-  private PolylineOptions createPolylineOptions() {
-    PolylineOptions options = new PolylineOptions();
-    options.addAll(coordinates);
-    options.color(color);
-    options.width(width);
-    options.geodesic(geodesic);
-    options.zIndex(zIndex);
-    return options;
+        LatLng coordinate = coordinates.get(i);
+        options.add(coordinate);
+
+        LatLng coordinate2 = coordinates.get(i+1);
+        options.add(coordinate2);
+
+        Integer colorToUse;
+
+        if((strokeColors != null) && (coordinates.size() == strokeColors.size())){
+            colorToUse = Color.parseColor(strokeColors.get(i));
+        } else {
+            colorToUse = color;
+        }
+
+        options.color(colorToUse);
+        options.width(width);
+        options.geodesic(geodesic);
+        options.zIndex(zIndex);
+
+        polyline = this.map.addPolyline(options);
+        polyline.setClickable(true);
+        this.polylineArray.add(i, polyline);
+    }
   }
 
   @Override
@@ -91,12 +142,16 @@ public class AirMapPolyline extends AirMapFeature {
 
   @Override
   public void addToMap(GoogleMap map) {
-    polyline = map.addPolyline(getPolylineOptions());
-    polyline.setClickable(true);
+    this.map = map;
+    createPolyline();
   }
 
   @Override
   public void removeFromMap(GoogleMap map) {
-    polyline.remove();
+    for (int i = 0; i < this.polylineArray.size(); i++) {
+        Polyline polyline = this.polylineArray.get(i);
+        polyline.remove();
+    }
+    this.polylineArray.clear();
   }
 }
